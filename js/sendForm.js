6d3c9934acdc245c93379b07cafff3bd90ec14bd
Olga Kolikhalova <kolikolga@yandex.ru>
2022-04-26 06:58:39 +0300

    очистка формы закрытие модального окна по клику на сабмит

--- js/sendForm.js
+++ js/sendForm.js
@@ -1,5 +1,38 @@
-const sendForm() => {
-  console.log('sendForm');
+const sendForm = () => {
+
+  const form = document.querySelector('.modal');
+  const modalBtn = document.querySelector('.modal__button');
+  const modal = document.querySelector('.modal');
+
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('submit');
+    const text = form.querySelector('input[type=text]');
+    const tel = form.querySelector('input[type=tel]');
+    const email = form.querySelector('input[type=email]');
+
+    const sendObj = {
+      name: text.value,
+      phone: tel.value,
+      email: email.value
+    };
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(sendObj),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      .finally(() => {
+        form.reset();
+        console.log('Форма очищена');
+        modal.style.display = '';
+      });
+  });
+
 };
 
 sendForm();
